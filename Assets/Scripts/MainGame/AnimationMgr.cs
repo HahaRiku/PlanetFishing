a28e6dce26e12e98300b18979c70113617bd0b95
Tsubaki520 <tsubaki520@gmail.com>
2021-10-23 21:12:55 +0800

    新增：主菜單的Logo、按鈕、貓貓、BGM; 調整：場景切換、動畫播放腳本

--- Assets/Scripts/MainGame/AnimationMgr.cs
+++ Assets/Scripts/MainGame/AnimationMgr.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class AnimationMgr : MonoBehaviour
 {
+    [SerializeField]
+    private Animator animator;
+
     void Start()
     {
         
+    }
+
+    public void Show統神()
+    {
+        animator.SetTrigger("統神");
+    }
+
+    public void Show冰冰姐()
+    {
+        animator.SetTrigger("冰冰姐");
+    }
+
+    public void Show瑞克搖()
+    {
+        animator.SetTrigger("瑞克搖");
+    }
+
+    public void Show蹦蹦姊()
+    {
+        animator.SetTrigger("蹦蹦姊");
     }
 }
--- Assets/Scripts/Scene/SceneMgr.cs
+++ Assets/Scripts/Scene/SceneMgr.cs
@@ -6,17 +6,10 @@ public class SceneMgr : MonoBehaviour
     public void ClickToStart()
     {
         SceneManager.LoadScene(1);
-        Debug.Log("點擊開始遊戲");
-    }
-
-    public void ClickOptions()
-    {
-        Debug.Log("點擊設定");
     }
 
     public void ClickToQuit()
     {
-        Debug.Log("點擊離開遊戲");
         Application.Quit();
     }
 }
