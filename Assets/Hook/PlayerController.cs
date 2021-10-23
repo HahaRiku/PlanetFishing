66fbb6daaf517d1655eb88411625fe0aa35cce2a
HuYeh <bibiyeh1999@gmail.com>
2021-10-23 13:32:27 +0800

    點擊定位

--- Assets/Hook/PlayerController.cs
+++ Assets/Hook/PlayerController.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject aaa;
+    public Vector2 PlayerPos2D;
+
+    public GameObject Aobj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPos2D = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Input.mousePosition;
-            Debug.Log(mousePos);
+            Vector3 pos = GetMousePosition();
+            pos.z = 20;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            ShootHook(pos);
+            Debug.Log(pos);
         }
         
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("wwww");
         }
+    }
+
+    public void ShootHook(Vector3 _mousePos)
+    {
+        GameObject temp = Instantiate(Aobj);
+        temp.transform.position = _mousePos;
+
+    }
+
+    public Vector3 GetMousePosition()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+
+        return mousePos;
     }
 }
