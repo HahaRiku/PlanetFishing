351e8984878e284161f053eaea40e9e8cb69ddc5
HahaRiku <s9323839@gmail.com>
2021-10-23 22:23:29 +0800

    調整 變成衛星後會縮小 且距離玩家保持一定距離

--- Assets/Scripts/PlanetMeme/CirclingRoute.cs
+++ Assets/Scripts/PlanetMeme/CirclingRoute.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CirclingRoute : MonoBehaviour {
+    private const float SATELLITE_DISTANCE = 0.1f;
     private List<GameObject> planetsInRoute = new List<GameObject>();
 
     // Start is called before the first frame update
@@ -17,6 +18,10 @@ public class CirclingRoute : MonoBehaviour {
 
     public void ReceiveNewPlanet(GameObject planet) {
         planet.transform.parent = transform;
+        Vector2 direction = new Vector2(planet.transform.localPosition.x, planet.transform.localPosition.y);
+        Vector2 newDirection = direction * (SATELLITE_DISTANCE / direction.magnitude);
+        planet.transform.localPosition = new Vector3(newDirection.x, newDirection.y, planet.transform.localPosition.z);
+        planet.transform.localScale *= 0.3f;
         planetsInRoute.Add(planet);
     }
 }
